2dea56d6a627399b2871276b9d130e0a97bfdee8
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2016-12-28 00:24:30 +0800

    electron updated.

--- electron/valkyrie.js
+++ electron/valkyrie.js
@@ -8,8 +8,6 @@ let win
 
 function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 , frame: false})
-
-  console.log(win.getNativeWindowHandle())
   valkyrie_addon.InitializeValkyrie(800, 600, win.getNativeWindowHandle())
 
   win.on('closed', () => {
