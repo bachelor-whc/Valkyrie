7709b7830866ddaee382567ba55112302d885d9b
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-06-13 19:26:01 +0800

    mutex and queue expand.

--- Valkyrie/source/scene/object_manager.cpp
+++ Valkyrie/source/scene/object_manager.cpp
@@ -21,14 +21,24 @@ void ObjectManager::close() {
 }
 
 ObjectManager::ObjectManager() : 
-	m_unused_ID(MAX_NUM_OF_OBJECTS),
+	m_unused_ID(DEFAULT_NUM_OF_OBJECTS),
 	m_used_ID(),
 	m_table(),
 	m_random_device(),
 	m_mt19937(m_random_device()) {
-	int n = 0;
+	++m_resize_times;
+	unsigned int n = 0;
 	std::generate(m_unused_ID.begin(), m_unused_ID.end(), [&n]() { return n++; });
 	std::shuffle(m_unused_ID.begin(), m_unused_ID.end(), m_mt19937);
+}
+
+void ObjectManager::expandQueue() {
+	if (m_unused_ID.empty()) {
+		unsigned int n = m_resize_times * DEFAULT_NUM_OF_OBJECTS;
+		m_resize_times += 1;
+		m_unused_ID.resize(DEFAULT_NUM_OF_OBJECTS);
+		std::generate(m_unused_ID.begin(), m_unused_ID.end(), [&n]() { return n++; });
+	}
 }
 
 ObjectManager::~ObjectManager() {
@@ -36,6 +46,9 @@ ObjectManager::~ObjectManager() {
 }
 
 unsigned int ObjectManager::acquireNextID() {
+	std::lock_guard<std::mutex> lock(m_registration_mutex);
+	if (m_unused_ID.empty())
+		expandQueue();
 	auto result = m_unused_ID.front();
 	m_unused_ID.pop_front();
 	m_used_ID.push_back(result);
@@ -49,11 +62,12 @@ int Valkyrie::ObjectManager::registerObject(const Scene::ObjectPtr& ptr) {
 	else if (m_table.count(ptr->getID()) != 0) {
 		return ALREADY_REGISTERED;
 	}
+	std::lock_guard<std::mutex> lock(m_registration_mutex);
 	m_table[ptr->getID()] = ptr;
 	return 0;
 }
 
-Scene::ObjectPtr ObjectManager::getObject(const unsigned int ID) {
+Scene::ObjectPtr ObjectManager::getObject(const unsigned int ID) const {
 	const auto& result = m_table.find(ID);
 	if (result == m_table.end())
 		return nullptr;
