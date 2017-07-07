5f54690647e3cb67a2d3b0f0011fd9f87597057a
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-07-07 08:33:24 +0800

    lights.

---
+++ Valkyrie/source/scene/light_directional.cpp
@@ -0,0 +1,10 @@
+#include "valkyrie/scene/light_directional.h"
+using namespace Valkyrie::Scene;
+
+DirectionalLight::DirectionalLight() : Light(Type::DIRECTIONAL) {
+
+}
+
+DirectionalLight::~DirectionalLight() {
+
+}
---
+++ Valkyrie/source/scene/light_position.cpp
@@ -0,0 +1,11 @@
+#include "valkyrie/scene/light_position.h"
+#include "valkyrie/vulkan/command_buffer.h"
+using namespace Valkyrie::Scene;
+
+PositionLight::PositionLight() : Light(Type::POSITION) {
+
+}
+
+PositionLight::~PositionLight() {
+
+}
